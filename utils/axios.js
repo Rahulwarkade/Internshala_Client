01110ff89e207149b8f2473d53091b577ed18a9a
SA India Consultancies <149512796+SAindia1@users.noreply.github.com>
2023-11-03 01:45:10 +0530

    Update axios.js

--- utils/axios.js
+++ utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create(
     {
-        baseURL : "http://api.jobzz24.com",
+        baseURL : "https://api.jobzz24.com",
         withCredentials : true,
     }
 );
